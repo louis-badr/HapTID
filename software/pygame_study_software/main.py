edd298edee739c11517b3befafdb1f0d3431d145
Louis BADR <louis.badr@edu.devinci.fr>
2023-06-30 19:06:54 +0200

    Calibration screen progress

--- software/pygame_study_software/main.py
+++ software/pygame_study_software/main.py
@@ -1,4 +1,15 @@
-import pygame, sys
+import numpy as np
+import os
+import plotly.graph_objects as go
+import pygame
+import sys
+
+id = input('Enter participant ID (ex:01BL):')
+#os.mkdir(f'./{id}')
+
+max_vib_lvl = 5
+nb_trials = 5
+threshold_value = None
 
 pygame.init()
 
@@ -11,6 +22,7 @@ screen_height = pygame.display.Info().current_h
 # Colors
 white = (255,255,255)
 black = (0,0,0)
+green = (0,255,0)
 
 # Fonts
 font = pygame.font.SysFont(None, 48)
@@ -30,28 +42,107 @@ def draw_button(text, font, color, surface, x, y):
     pygame.draw.rect(surface, white, (textrect.left-10, textrect.top-10, textrect.width+20, textrect.height+20), 2)
     return textrect
 
-def main_menu():
-    pygame.display.set_caption("Menu - HapTID")
-    while True:
+def calib_screen(id):
+    pygame.display.set_caption("Calibration - HapTID")
+    screen.fill((0,0,0))
+    menu_button = draw_button('Menu', font, white, screen, 100, 50)
+    draw_text('Avez-vous senti une vibration ?', font, white, screen, screen_width/2, screen_height/2)
+    pygame.display.update()
+    pygame.time.wait(2000)
+
+    vib_lvl = max_vib_lvl
+    vib_lvl_history = []
+    changing_points = []
+    answers_history = []
+    step = 1.5
+    coeff = 0.7
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                if menu_button.collidepoint(event.pos):
+                    running = False
+                    main_menu(id)
+                if yes_button.collidepoint(event.pos) or no_button.collidepoint(event.pos):
+                    print(answers_history)
+                    print(vib_lvl_history)
+                    if len(vib_lvl_history) >= nb_trials:
+                        print(vib_lvl_history)
+                        print(changing_points)
+                        threshold_value = np.mean(changing_points)
+                        print(threshold_value)
+                        running=False
+                        main_menu(id)
+                    screen.fill((0,0,0))
+                    menu_button = draw_button('Menu', font, white, screen, 100, 50)
+                    draw_text('Avez-vous senti une vibration ?', font, white, screen, screen_width/2, screen_height/2)
+                    pygame.display.update()
+                    pygame.time.wait(2000)
+                    # vibrate here at vib_lvl
+                    pygame.time.wait(250)
+                    if yes_button.collidepoint(event.pos):
+                        if len(answers_history) == 0 or answers_history[-1] == 'y':
+                            vib_lvl_history.append(vib_lvl)
+                            answers_history.append('y')
+                            vib_lvl -= step
+                        elif answers_history[-1] == 'n':
+                            vib_lvl_history.append(vib_lvl)
+                            answers_history.append('y')
+                            changing_points.append(vib_lvl)
+                            step *= coeff
+                            vib_lvl -= step
+                        if vib_lvl < 0:
+                            vib_lvl = 0
+                    if no_button.collidepoint(event.pos):
+                        if answers_history[-1] == 'n':
+                            vib_lvl_history.append(vib_lvl)
+                            answers_history.append('n')
+                            vib_lvl += step
+                        elif answers_history[-1] == 'y':
+                            vib_lvl_history.append(vib_lvl)
+                            answers_history.append('n')
+                            changing_points.append(vib_lvl)
+                            step *= coeff
+                            vib_lvl += step
+                        if vib_lvl > max_vib_lvl:
+                            vib_lvl = max_vib_lvl
+
+        screen.fill((0,0,0))
+        # always displayed
+        menu_button = draw_button('Menu', font, white, screen, 100, 50)
+        draw_text('Avez-vous senti une vibration ?', font, white, screen, screen_width/2, screen_height/2)
+        no_button = draw_button('Non', font, white, screen, screen_width/2-100, screen_height/2+100)
+        yes_button = draw_button('Oui', font, white, screen, screen_width/2+100, screen_height/2+100)
+        
+
+        pygame.display.update()
+        clock.tick(60)
+
+def main_menu(id):
+    pygame.display.set_caption("Home - HapTID")
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type==pygame.MOUSEBUTTONDOWN:
                 if calib_button.collidepoint(event.pos):
-                    print('Calibration')
-                if crt_button.collidepoint(event.pos):
-                    print('Choice Reaction Time')
-                if fc_button.collidepoint(event.pos):
-                    print('Force Control')
+                    running = False
+                    calib_screen(id)
 
         screen.fill((0,0,0))
-        draw_text('Main Menu', font, white, screen, screen_width/2, screen_height/2-200)
-        calib_button = draw_button('Calibration', font, white, screen, screen_width/2, screen_height/2-100)
-        crt_button = draw_button('Choice Reaction Time', font, white, screen, screen_width/2, screen_height/2)
-        fc_button = draw_button('Force Control', font, white, screen, screen_width/2, screen_height/2+100)
+        draw_text(f'Participant {id}', font, white, screen, screen_width/2, screen_height/2-100)
+        calib_button = draw_button('Start calibration', font, white, screen, screen_width/2, screen_height/2)
+        if threshold_value is not None:
+            exercices_button = draw_button('Start exercices', font, white, screen, screen_width/2, screen_height/2+100)
+            draw_text('Calibration complete !', font, green, screen, screen_width/2, screen_height/2+200)
 
         pygame.display.update()
         clock.tick(60)
 
-main_menu()
+main_menu(id)
