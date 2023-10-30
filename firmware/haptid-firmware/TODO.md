476e52e45180966108778888cf7a022118770c69
Louis BADR <louis.badr@edu.devinci.fr>
2023-10-30 16:27:41 +0100

    Cleaned up pins management

---
+++ firmware/haptid-firmware/TODO.md
@@ -0,0 +1,5 @@
+# to do list
+
+- [ ] Play audio files other than alice.h
+- [ ] Get volume control
+- [ ] Get simultaneous multioutput
--- firmware/haptid-firmware/haptid-firmware.ino
+++ firmware/haptid-firmware/haptid-firmware.ino
@@ -1,25 +1,16 @@
 #include "AudioTools.h"
 #include "audiofile.h"
-//#include "alice.h"
+#include "alice.h"
 
-unsigned long starttime;
-
-// keep shdn on HIGH to run the amp - each amp controls two motors
-int shdn1 = 22;
-int motor1 = 32;
-int motor2 = 33;
-
-int shdn2 = 19;
-int motor3 = 25;
-int motor4 = 26;
-
-int shdn3 = 4;
-int motor5 = 27;
-int motor6 = 12;
+// keep pin on HIGH to run the amp - each amp controls two motors
+int shdnPins[3] = {22, 19, 4};
+// in order from left to right
+int motorPins[6] = {32, 33, 25, 26, 27, 12};
+// tracks if audio is playing on a motor
+bool motorIsPlaying[6] = {0, 0, 0, 0, 0, 0};
 
 //Data Flow: MemoryStream -> EncodedAudioStream  -> PWMAudioOutput
-
-AudioInfo info(8000, 1, 8);
+AudioInfo info(8000, 1, 16);
 
 SineWaveGenerator<int16_t> sineWave(32000); // subclass of SoundGenerator with max amplitude of 32000
 GeneratedSoundStream<int16_t> sound(sineWave);  // Stream generated from sine wave
@@ -28,45 +19,24 @@ MemoryStream wav(audiofile_raw, audiofile_raw_len);
 PWMAudioOutput pwm;          // PWM output 
 EncodedAudioStream out(&pwm, new WAVDecoder()); // Decoder stream
 
-void setup(){
-  Serial.begin(115200);
-
-  pinMode(shdn1, OUTPUT);
-  pinMode(motor1, OUTPUT);
-  pinMode(motor2, OUTPUT);
-
-  pinMode(shdn2, OUTPUT);
-  pinMode(motor3, OUTPUT);
-  pinMode(motor4, OUTPUT);
-
-  pinMode(shdn3, OUTPUT);
-  pinMode(motor5, OUTPUT);
-  pinMode(motor6, OUTPUT);
-
-  digitalWrite(shdn1, HIGH);
-  digitalWrite(motor1, LOW);
-  digitalWrite(motor2, LOW);
-
-  digitalWrite(shdn2, HIGH);
-  digitalWrite(motor3, LOW);
-  digitalWrite(motor4, LOW);
-
-  digitalWrite(shdn3, HIGH);
-  digitalWrite(motor5, LOW);
-  digitalWrite(motor6, LOW);
-
-  AudioLogger::instance().begin(Serial, AudioLogger::Info);
-  wav.setLoop(true);
-  //playTone(motor6, 1, 5000, 440);
-  playAudioFile(motor6, 1, 5000);
-}
-
-void loop(){
-
-}
+// void startPlayAudioFile(int motor, float volume) {
+//   Stream copier(out, wav);
+//   out.begin();
+//   wav.begin();
+//   auto config = pwm.defaultConfig();
+//   config.copyFrom(info);
+//   config.resolution = 11;
+//   config.start_pin = motor;
+//   pwm.begin(config); 
+//   while (millis() - starttime <= duration) {
+//     copier.copy();
+//   }
+//   digitalWrite(motor, LOW);
+// }
 
 // plays audio file on the selected motor - volume 0-1 - duration in ms 0-30000
-void playAudioFile(int motor, float volume, float duration){
+void playAudioFile(int motor, float volume, float duration) {
+  unsigned long starttime = millis();
   StreamCopy copier(out, wav);    // copy in to out
   out.begin();   // indicate that we process the WAV header
   wav.begin();       // reset actual position to 0
@@ -75,7 +45,6 @@ void playAudioFile(int motor, float volume, float duration){
   config.resolution = 11;
   config.start_pin = motor;
   pwm.begin(config); 
-  starttime = millis();
   while (millis() - starttime <= duration) {
     copier.copy();
   }
@@ -83,15 +52,17 @@ void playAudioFile(int motor, float volume, float duration){
 }
 
 // plays a click on the selected motor 1-6 with a volume value 0-1
-void playClick(int motor, float volume){
+void playClick(int motor, int volume) {
+  unsigned long starttime = millis();
   volume *= 255;
   digitalWrite(motor, volume);
-  delay(8);
+  while (millis() - starttime <= 10) {}
   digitalWrite(motor, LOW);
 }
 
 // plays a tone on the selected motor - frequency (Hz) 0-20k - volume 0-1 - duration in ms 0-30000
-void playTone(int motor, float volume, float duration, int frequency){
+void playTone(int motor, float volume, float duration, int frequency) {
+  unsigned long starttime = millis();
   StreamCopy copier(pwm, sound);
   sineWave.begin(info, frequency);
   auto config = pwm.defaultConfig();
@@ -99,9 +70,49 @@ void playTone(int motor, float volume, float duration, int frequency){
   config.resolution = 11;
   config.start_pin = motor;
   pwm.begin(config);
-  starttime = millis();
   while (millis() - starttime <= duration) {
     copier.copy();
   }
   digitalWrite(motor, LOW);
+}
+
+void playStartingSequence() {
+  for (int i = 0; i <= 5; i++) {
+    playClick(motorPins[i], 1);
+    delay(250);
+  }
+  delay(250);
+  for (int i = 5; i >= 0; i--) {
+    playClick(motorPins[i], 1);
+    delay(250);
+  }
+  delay(750);
+  for (int i = 0; i <= 5; i++) {
+    playClick(motorPins[i], 1);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  for (int i = 0; i <= 2; i++) {
+    pinMode(shdnPins[i], OUTPUT);
+    digitalWrite(shdnPins[i], HIGH);
+  }
+  for (int i = 0; i <= 5; i++) {
+    pinMode(motorPins[i], OUTPUT);
+    digitalWrite(motorPins[i], LOW);
+  }
+
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+  wav.setLoop(true);
+
+  delay(5000);
+  playStartingSequence();
+  delay(1000);
+  //playAudioFile(motor6, 1, 5000);
+}
+
+void loop() {
+
 }
