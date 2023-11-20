619a94116d389585038678e865ab7820daa15621
Louis BADR <louis.badr@edu.devinci.fr>
2023-11-20 17:06:39 +0100

    Study firmware progress

--- firmware/haptid-firmware-study-2/haptid-firmware-study-2.ino
+++ firmware/haptid-firmware-study-2/haptid-firmware-study-2.ino
@@ -1,30 +1,28 @@
 #include "AudioTools.h"
 #include "whitenoise8000.h"
 
-// keep pin on HIGH to run the amp - each amp controls two motors
-int shdnPins[3] = {22, 19, 4};
-// in order from left to right
-int motorPins[6] = {32, 33, 25, 26, 27, 12};
 
-//Data Flow: MemoryStream -> EncodedAudioStream  -> PWMAudioOutput
+int shdnPins[3] = {22, 19, 4};  // keep pin on HIGH to run the amp - each amp controls two motors
+int motorPins[6] = {32, 33, 25, 26, 27, 12};  // in order from left to right
+
+int receivedValue = 0;  // single int value received from serial between 0 and 506
+bool playNoise = false;
+bool playTone = false;
+unsigned long clickTimer;
+bool isClicking = false;
+int motorClicking;
+
 AudioInfo info(8000, 1, 16);
 SineWaveGenerator<int16_t> sineWave(32000); // subclass of SoundGenerator with max amplitude of 32000
-//GeneratedSoundStream<int16_t> tone(sineWave);  // Stream generated from sine wave
+GeneratedSoundStream<int16_t> sineTone(sineWave);  // Stream generated from sine wave
 MemoryStream wav(wav_data, wav_data_len);
 PWMAudioOutput pwm;          // PWM output 
 EncodedAudioStream out(&pwm, new WAVDecoder()); // Decoder stream
 
-// on crée un copier pour chaque type de son - un pour le bruit, un pour les sinusoïdes
+StreamCopy copierTone(pwm, sineTone);
 StreamCopy copierWav(out, wav);
-PWMConfig config = pwm.defaultConfig();
-//StreamCopy copierTone(pwm, tone);
-
-int receivedValue = 0;
-unsigned long clickTimer;
-bool playNoise = false;
-bool playLowTone = false;
-bool playHighTone = false;
-bool isClicking = false;
+PWMConfig configTone = pwm.defaultConfig();
+PWMConfig configWav = pwm.defaultConfig();
 
 void setup() {
   Serial.begin(115200);
@@ -40,48 +38,66 @@ void setup() {
     digitalWrite(motorPins[i], LOW);
   }
   AudioLogger::instance().begin(Serial, AudioLogger::Info);
+  out.begin();
   wav.setLoop(true);
-  out.begin();   // indicate that we process the WAV header
-  wav.begin();       // reset actual position to 0
+  wav.begin();
+  configTone.sample_rate = 8000;
+  configTone.channels = 1;
+  configTone.bits_per_sample = 16;
+  configTone.resolution = 11;
+  configTone.start_pin = motorPins[5];
+  configWav.sample_rate = 8000;
+  configWav.channels = 1;
+  configWav.bits_per_sample = 16;
+  configWav.resolution = 11;
+  configWav.start_pin = motorPins[0];
   delay(3000);
 }
 
 void loop() {
   if (Serial.available() > 0) {
     receivedValue = Serial.parseInt();
-    Serial.println(receivedValue);
     // 501 to 506 are clicks - left to right
     if (receivedValue >= 501 && receivedValue <= 506) {
-      digitalWrite(motorPins[receivedValue - 500], HIGH);
+      digitalWrite(motorPins[receivedValue - 501], HIGH);
       clickTimer = millis();
       isClicking = true;
+      motorClicking = receivedValue - 501;
     }
-    // 200 to 300 for volume of low frequency tone on index finger
-    if (receivedValue >= 200 && receivedValue <= 300) {}
+    // 200 to 300 or 400 to 500 for volume of low frequency tone on index finger
+    if (receivedValue >= 200 && receivedValue <= 500) {
+      if (receivedValue == 200 || receivedValue == 400) {
+        digitalWrite(motorPins[5], LOW);
+        playTone = false;
+      } else if (receivedValue <= 300) {
+        sineWave.begin(info, 140);
+        pwm.begin(configTone);
+        playTone = true;
+      } else if (receivedValue <= 500) {
+        sineWave.begin(info, 200);
+        pwm.begin(configTone);
+        playTone = true;
+      }
+    }
     // 0 to 100 for volume of noise on motor 1
-    if (receivedValue > 0 && receivedValue <= 100) {
-      config.sample_rate = 8000;
-      config.channels = 1;
-      config.bits_per_sample = 16;
-      config.resolution = 11;
-      playNoise = true;
-      config.start_pin = motorPins[0];
-      pwm.begin(config); 
-    }
-
-    // stop noise on motor 1
-    if (receivedValue == 0) {
-      Serial.println("Stop noise");
-      playNoise = false;
-      digitalWrite(motorPins[0], LOW);
+    if (receivedValue >= 0 && receivedValue <= 100) {
+      if (receivedValue == 0) {
+        digitalWrite(motorPins[0], LOW);
+        playNoise = false;
+      } else {
+        pwm.begin(configWav);
+        playNoise = true; 
+      }
     }
   }
-  // if noise is activated, play noise on motor 1
   if (playNoise) {
     copierWav.copy();
   }
+  if (playTone) {
+    copierTone.copy();
+  }
   if (isClicking && millis() - clickTimer >= 8) {
-    digitalWrite(motorPins[1], LOW);
+    digitalWrite(motorPins[motorClicking], LOW);
     isClicking = false;
   }
 }
